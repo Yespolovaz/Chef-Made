6b127d807a5bc16834fb2befbf068f81e2845512
Yespolovaz <z_espolova@kbtu.kz>
2023-04-21 03:27:53 +0600

    upd

---
+++ chef-made/src/app/categories/categories.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {CategoryService} from "../services/category/category.service";
+import {Category} from "../../models/category";
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit{
+  categories:Category[] = []
+  ngOnInit(): void {
+    this.categories = this.categoryService.getAll();
+  }
+  constructor(private categoryService:CategoryService) {
+  }
+
+}
---
+++ chef-made/src/models/food.ts
@@ -0,0 +1,9 @@
+export interface Food{
+  id:number;
+  name:string
+  ingredients:string[];
+  favorite:boolean;
+  imageUrl:string;
+  cookTime:string;
+  description:string;
+}
